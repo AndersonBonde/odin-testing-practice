89fe51defa52e15f866503d894c53b36a66133f2
Anderson <andersonbonde@gmail.com>
2023-11-21 10:51:10 -0300

    Write reverseString tests

---
+++ src/tests/reverseString.test.js
@@ -0,0 +1,13 @@
+import reverseString from '../functions/reverseString';
+
+test('abc to equal cba', () => {
+  expect(reverseString('abc')).toBe('cba');
+})
+
+test('word to equal drow', () => {
+  expect(reverseString('word')).toBe('drow');
+});
+
+test(`Hello, I'm under the water. to equal .retaw eht rednu m'I ,olleH`, () => {
+  expect(reverseString(`Hello, I'm under the water.`)).toBe(`.retaw eht rednu m'I ,olleH`);
+});
